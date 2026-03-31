6eb441b27f27534511b302b5b2d7da4585b3c5d7
PhanThao84 <phanthithanhthao842004123@gmail.com>
2026-03-31 20:19:09 +0700

    Commit 31/3

--- QTTTTN/TruongBoMon/urls.py
+++ QTTTTN/TruongBoMon/urls.py
@@ -4,4 +4,7 @@ app_name = 'TruongBoMon'
 urlpatterns = [
     path('', views.home_view, name='truongboomon_home'),
     path('phan-cong-gvpt/', views.phan_cong_gvpt_view, name='phan_cong_gvpt'),
+    path('duyet-gvhd/', views.duyet_gvhd_view, name='duyet_gvhd'),
+    path('duyet-gvhd/action/', views.action_duyet_gvhd, name='action_duyet_gvhd'),
+    path('duyet-gvhd/<str:ma_gv>/', views.chi_tiet_gvhd_view, name='chi_tiet_gvhd'),
 ]
